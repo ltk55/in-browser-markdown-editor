27688f4327cc15d902b12505a635e3128408b3ca
Frank Lam <frankltk55@gmail.com>
2023-10-25 12:10:18 +0100

    style: modify lightmode header text colour

--- src/components/header/DeleteDocumentModal.tsx
+++ src/components/header/DeleteDocumentModal.tsx
@@ -22,13 +22,13 @@ export default function DeleteDocumentModal({
     <div
       className={`${
         isModalOpen ? "block" : "hidden"
-      }  absolute left-0 top-0 z-50 h-screen w-screen bg-overlayTransparent dark:bg-lightOverlayTransparent`}
+      }  dark:bg-lightOverlayTransparent absolute left-0 top-0 z-50 h-screen w-screen bg-overlayTransparent`}
       onClick={() => setIsModalOpen(false)}
     >
       <div className="absolute left-1/2 top-1/2 w-[343px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-neutral-100 p-6 dark:bg-neutral-1000">
-        <span className="font-serif text-xl font-bold text-neutral-100 dark:text-neutral-100">
+        <h4 className="font-serif text-xl font-bold text-neutral-700 dark:text-neutral-100">
           Delete this document?
-        </span>
+        </h4>
         <p className="my-4 font-serif text-sm font-normal text-neutral-500 dark:text-neutral-400">
           Are you sure you want to delete the ‘welcome.md’ document and its
           contents? This action cannot be reversed.
